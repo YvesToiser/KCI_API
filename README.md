3b34395de519a8a6676b39ff4782d37b0c1441ee
YvesToiser <yves.toiser@gmail.com>
2025-02-07 15:05:03 +0100

    request for yes/no

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # KCI_API
 Flask API checking if Kingsley Coman is injured
+
+For local test : http://127.0.0.1:5000/KC
+
---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route('/KC', methods=['GET'])
+def KC():
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0"
+    }
+    url = "https://www.transfermarkt.fr/kingsley-coman/verletzungen/spieler/243714/plus/1"
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        zentriert_cells = soup.find_all("td", attrs={"class": "zentriert"})
+        end_last = zentriert_cells[2].text.strip()
+        if end_last == "":
+            return "yes"
+        else:
+            return "no"
+    else:
+        return "request_failed"
+
+
+if __name__ == '__main__':
+    app.run()
