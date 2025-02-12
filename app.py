bd3265a55c477379fe57f10a1be5b982bf144a4f
YvesToiser <yves.toiser@gmail.com>
2025-02-12 16:55:30 +0100

    add CORS management

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import requests
 from bs4 import BeautifulSoup
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+CORS(app)
+WHITELISTED_IPS = ["127.0.0.1", ""]
+
 @app.route('/KC', methods=['GET'])
 def KC():
+    client_ip = request.remote_addr
+    if client_ip not in WHITELISTED_IPS:
+        return jsonify({"error": "Unauthorized"}), 403
+
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0"
     }
     url = "https://www.transfermarkt.fr/kingsley-coman/verletzungen/spieler/243714/plus/1"
     response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, "html.parser")
         zentriert_cells = soup.find_all("td", attrs={"class": "zentriert"})
